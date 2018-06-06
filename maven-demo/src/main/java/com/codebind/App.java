469609df447fec153de18ef21c21d775c9c0336f
nmietus <nmietus93@gmail.com>
2018-06-06 17:18:43 +0200

    Maven demo

---
+++ maven-demo/src/main/java/com/codebind/App.java
@@ -0,0 +1,15 @@
+package com.codebind;
+
+public class App {
+		
+		public static void main(String[] args) 
+		{
+			System.out.println("test");
+		}
+		
+		public String Sample() 
+		{
+			return "sample";
+		}
+		
+}
